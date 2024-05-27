ebb8cc8879db1338abf04c75b24d4dfe483dac30
Joon Lee <Joon.S.Lee@ibm.com>
2024-05-27 15:03:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/ZOSOpenTools/s5cmdport/actions/workflows/bump.yml/badge.svg)](https://github.com/ZOSOpenTools/s5cmdport/actions/workflows/bump.yml)
+
+# s5cmd
+
+Parallel S3 and local filesystem execution tool.
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/s5cmdport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/s5cmdport.git'), string(name: 'PORT_DESCRIPTION', value: 'Parallel S3 and local filesystem execution tool.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
