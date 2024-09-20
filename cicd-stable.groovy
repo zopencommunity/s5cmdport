d2d41c72f8df850fe4e074677af821fdaf0448e7
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:51 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/s5cmdport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/s5cmdport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/s5cmdport.git'), string(name: 'PORT_DESCRIPTION', value: 'Parallel S3 and local filesystem execution tool.' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "go_120" ) ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/s5cmdport.git'), string(name: 'PORT_DESCRIPTION', value: 'Parallel S3 and local filesystem execution tool.' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "go_120" ) ]
   }
 }
